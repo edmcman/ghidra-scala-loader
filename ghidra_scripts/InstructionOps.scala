feba918e721f427e25b885e682cfdff932131b3c
Edward J. Schwartz <eschwartz@cert.org>
2023-10-27 17:02:42 -0400

    Add new example script

---
+++ ghidra_scripts/InstructionOps.scala
@@ -0,0 +1,43 @@
+// This script dumps various instruction operand information to help understand them.
+// @author Ed Schwartz <eschwartz@cert.org>
+// @category Functions
+
+import ghidra.app.script.GhidraScript
+import ghidra.program.model.listing.Function
+import ghidra.program.model.address.AddressSetView
+import ghidra.program.model.symbol.Reference
+
+import scala.collection.JavaConverters._
+
+class InstructionOps extends GhidraScript {
+
+  override def run() = {
+
+
+    println("Starting InstructionOps script")
+    
+    val funcs = getCurrentProgram.getFunctionManager
+      .getFunctions(true)
+      .asInstanceOf[java.lang.Iterable[Function]]
+      .asScala
+
+    val l = getCurrentProgram.getListing
+
+    funcs.foreach(f => {
+      println(s"Function ${f.getName}")
+      l.getInstructions(f.getBody, true).forEach(i => {
+        val numops = i.getNumOperands
+        println(s" Insn: ${i} has ${numops} operands")
+        (0 to numops-1).foreach(opnum => {
+          println(s"  Operand ${opnum} ${i.getDefaultOperandRepresentation(opnum)}")
+          //println(s"  Operand ${opnum} is ${i.getOpObjects(opnum)}")
+          val reftype = i.getOperandRefType(opnum)
+          println(s"  Operand ${opnum} ref type ${reftype}")
+          val optype = i.getOperandType(opnum)
+          val optypestr = ghidra.program.model.lang.OperandType.toString(optype)
+          println(s"  Operand ${opnum} op type ${optypestr}")
+        })
+      })
+    })
+  }
+}
